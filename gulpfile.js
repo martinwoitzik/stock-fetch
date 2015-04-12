95eedbac061a7b15ca7eb4b82516cf9602adf5d1
Martin Woitzik <m.woitzik@gmx.de>
2015-04-12 23:26:40 +0200

    Refactor folder structure. Add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
@@ -6,16 +7,17 @@ var uglify = require('gulp-uglify');
 
 var config = {
   base: './',
+  js: 'app/**/*.js',
+  libs: 'libs/**/*',
   sass: 'sass/**/*.scss',
   dist: {
-    bundle: 'dist/js/app.min.js',
     css: 'dist/css/',
     images: 'dist/images',
-    scripts: 'dist/js'
+    js: 'dist/js'
   }
 };
 
-gulp.task('build', ['css'], function() {
+gulp.task('build', ['css', 'js'], function() {
 });
 
 gulp.task('watch', ['build'], function () {
@@ -25,11 +27,20 @@ gulp.task('watch', ['build'], function () {
 gulp.task('css', function () {
   gulp.src(config.sass)
     .pipe(plumber())
-    .pipe(sourcemaps.init())
-    // Compress SCSS during production build.
+    //.pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     }))
-    .pipe(sourcemaps.write('.'))
+    //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.dist.css));
+});
+
+gulp.task('js', function() {
+  gulp.src(config.js)
+    .pipe(plumber())
+    //.pipe(sourcemaps.init())
+    //.pipe(concat())
+    .pipe(uglify())
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.dist.js));
 });
